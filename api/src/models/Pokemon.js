3ed1f8ec3ab4981ed5ad3712bdebe2aed73a6c6b
jaidan78 <jaime.baldoma78@gmail.com>
2023-06-26 19:40:15 -0300

    26.06.2023 19:40

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,22 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Life: {
+
+    },
+
   });
 };
